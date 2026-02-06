500fd9c450191b398981d5fc5591598e26a2a999
Ivan Montejo de Garcini <elburgoimontejo@gmail.com>
2026-02-06 13:22:17 +0200

    diched the idea of storing the vectors in the database, they are just a pickle object that can be loaded. the function to load the vectors (tfidf and boolean have each their fucntion) is now in the database.py. The function to get all game data from the database can now be restricted to only include a restricted set of ids, just use the ids keyword argument by passing any list-like.

--- src/testQuerydb.py
+++ src/testQuerydb.py
@@ -19,7 +19,7 @@ pprint(cur.execute("""
                   """).fetchall())
 print("NEW QUERY")
 pprint(cur.execute("""
-                    SELECT g.name as games, GROUP_CONCAT(gen.name, ', '), g.metacritic as genres FROM 
+                    SELECT g.id_game, g.name as games, GROUP_CONCAT(gen.name, ', '), g.metacritic as genres FROM 
                     genre gen NATURAL JOIN has_genre x
                     JOIN game g ON x.id_game == g.id_game
                     WHERE   g.metacritic > 80 AND
@@ -38,28 +38,6 @@ pprint(cur.execute("""
                     ORDER BY AVG(g.rating) DESC
                     LIMIT 10
                   """).fetchall())
-
-print("NEW QUERY")
-pprint(cur.execute("""
-                    SELECT 
-                    g.id_game as id_game,
-                    g.name as name,
-                    GROUP_CONCAT(gen.name, ', ') as genres,
-                    g.description,
-                    GROUP_CONCAT(d.name, ', ') as developers,
-                    GROUP_CONCAT(p.name, ', ') as publishers
-                    FROM game g NATURAL JOIN developed
-                    JOIN company d ON d.id_company == developed.id_company
-                    JOIN has_genre x ON x.id_game == g.id_game
-                    JOIN genre gen ON gen.id_genre == x.id_genre
-                    JOIN published y ON y.id_game == g.id_game
-                    JOIN company p ON p.id_company == y.id_company
-                    GROUP BY g.id_game
-                    ORDER BY g.metacritic
-                    LIMIT 10
-                  """).fetchall())
-print("NEW QUERY")
-pprint(db.get_text_gamedata(limit=1))
 
 
 conn.close()
--- src/vector_processing.py
+++ src/vector_processing.py
@@ -22,42 +22,17 @@ def batch_insert(tablename: str ,db_cur:sql.Cursor, db_conn:sql.Connection, tupl
     """, tuples)
 
 
-"""if input("\033[31mWARNING: This action will remove and reset the vectors from the database. Do you REALLY want to do it?(yes/no)\033[0m") != "yes":
-    raise(KeyboardInterrupt("Execution was manually interrupted."))"""
+if input("\033[31mWARNING: This action will remove and reset the vectors from the database. Do you REALLY want to do it?(yes/no)\033[0m") != "yes":
+    raise(KeyboardInterrupt("Execution was manually interrupted."))
 
 path = Path(os.path.abspath(os.path.dirname(__file__)))
 db_path = path / ".." / "data" / "games.db"
 json_path = path / ".." / "data" / "db.json"
 conn = db.conn  #sql.connect(db_path)
 cur = db.cur    #conn.cursor()
+data_path = path / ".." / "data"
 
 cur.execute("BEGIN") # Begin transaction
-
-# Remove boolean table table
-cur.execute("""
-    DROP TABLE IF EXISTS boolean_vector
-            """)
-
-# Remove tf-idf table table
-cur.execute("""
-    DROP TABLE IF EXISTS tf_idf
-            """)
-
-# (re)create boolean table table and tfidf
-cur.execute("""
-    CREATE TABLE boolean_vector (
-    id_game INT PRIMARY KEY,
-    embedding BLOB,
-    FOREIGN KEY (id_game) REFERENCES game(id_game) DEFERRABLE INITIALLY DEFERRED
-    )
-            """)
-cur.execute("""
-    CREATE TABLE tfidf (
-    id_game INT PRIMARY KEY,
-    embedding BLOB,
-    FOREIGN KEY (id_game) REFERENCES game(id_game) DEFERRABLE INITIALLY DEFERRED
-    )
-            """)
 
 data = pd.read_sql(db.get_text_gamedata(as_text=True), conn, index_col="id_game")
 
@@ -78,18 +53,8 @@ for col in data.columns:
                 index=data.index,
                 columns=cv.get_feature_names_out()
                 )
-insert_data = {}
+pickle.dump(vector_matrices, open(data_path / "boolean_vectors", "wb"))
 
-for id in range(len(data.index)):
-    insert_data[data.index[id]] = {}
-    for field in vector_matrices.keys():
-        insert_data[data.index[id]][field] = vector_matrices[field].loc[data.index[id]]
-    insert_data[data.index[id]] = pickle.dumps(insert_data[data.index[id]])
-
-tuples = [(int(k), v) for k, v in insert_data.items()]
-
-batch_insert("boolean_vector", cur, conn, tuples)
-print("Boolean vectors done")
 ####### TF-IDF VECTORS ###########
 docs = pd.Series([". ".join(data.loc[i]) for i in data.index], index=data.index)
 vectorizer = TfidfVectorizer(
@@ -106,25 +71,13 @@ tfidf_matrix = pd.DataFrame.sparse.from_spmatrix(
                     index=docs.index, 
                     columns=vectorizer.get_feature_names_out()
                     )
+pickle.dump(tfidf_matrix, open(data_path / "tf_idf", "wb"))
 
-tuples = []
-
-for i in docs.index:
-    compressed_vector = pickle.dumps(tfidf_matrix.loc[i])
-    tuples.append((i, compressed_vector))
-
-batch_insert("tfidf", cur, conn, tuples)
 print("tf-idf vectors done")
 
 ####### TESTING #######
-# Boolean testing
 t = time.time()
-result = cur.execute("""
-    SELECT id_game, embedding FROM boolean_vector
-""").fetchall()
-print(f"Fetching compressed boolean vectors took {time.time()-t} seconds")
-t = time.time()
-data = db.remake_boolean_matrix(cur)
+data = db.load_pickle_file(data_path / "boolean_vectors")
 print(f"decompressing and remaking boolean vector matrix took {time.time()-t} seconds")
 print("Sanity check")
 for i in np.random.choice(data[list(data.keys())[0]].index, size=5, replace=False):
@@ -144,12 +97,7 @@ for i in np.random.choice(data[list(data.keys())[0]].index, size=5, replace=Fals
 
 # tf-idf testing
 t = time.time()
-result = cur.execute("""
-    SELECT id_game, embedding FROM tfidf
-""").fetchall()
-print(f"Fetching tf-idf vectors took {time.time()-t} seconds")
-t = time.time()
-data = db.remake_tfidf_matrix()
+data = db.load_pickle_file(data_path / "tf_idf")
 print(f"decompressing and remaking tf-idf vector matrix took {time.time()-t} seconds")
 print(data.head())
 print("Sanity check")
@@ -167,6 +115,5 @@ for i in np.random.choice(data.index, size=5, replace=False):
         print("values equal:", np.all(truth.values == remade.values))
         print("Same class?", truth.__class__ == remade.__class__)
 
-raise(ValueError)
 conn.commit()
 conn.close()
